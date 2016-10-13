0b11344a5a4a08a4e099b2f8883e60b5c7124e4b
Dzheni Dilcheva <dzhenidi@buffalo.edu>
2016-10-13 13:30:44 -0400

    add create comment to todo reducer

--- frontend/reducers/todo_reducer.js
+++ frontend/reducers/todo_reducer.js
@@ -3,6 +3,7 @@ import { RECEIVE_TODOS,
          REMOVE_TODO,
          TODO_ERROR
        } from '../actions/todo_actions';
+import { RECEIVE_COMMENT } from '../actions/comments_actions';
 
 const TodosReducer = (state = {}, action) => {
   switch(action.type){
@@ -11,7 +12,7 @@ const TodosReducer = (state = {}, action) => {
       action.todos.forEach(todo => newState[todo.id] = todo);
       return newState;
     case RECEIVE_TODO:
-      const newTodo = {[action.todo.id]: action.todo};
+      let newTodo = {[action.todo.id]: action.todo};
       return Object.assign({}, state, newTodo);
     case REMOVE_TODO:
       newState = Object.assign({}, state);
@@ -20,6 +21,12 @@ const TodosReducer = (state = {}, action) => {
     case TODO_ERROR:
       alert(action.error);
       break;
+    case RECEIVE_COMMENT:
+      let todoId = action.comment.commentable_id;
+      newTodo = Object.assign({}, state[todoId]);
+      let newCommentIds = newTodo.commentIds.concat(action.comment.id);
+      newTodo.commentIds = newCommentIds;
+      return Object.assign({}, state, {[todoId]: newTodo});
     default:
       return state;
   }
