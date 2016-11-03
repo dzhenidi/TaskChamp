3615450cf6c8f942d138b90a15b280ddeb446665
Dzheni Dilcheva <dzhenidi@buffalo.edu>
2016-11-03 15:02:22 -0400

    sort todos for schedule view

--- frontend/components/schedule/schedule_container.js
+++ frontend/components/schedule/schedule_container.js
@@ -1,20 +1,18 @@
 import { connect } from 'react-redux';
 import { requestTodos } from '../../actions/todo_actions';
 import Schedule from './schedule';
+import { sortedAscending } from '../../reducers/selectors';
 
 const mapStateToProps = ({todo}) => {
-  let todosIdx = Object.keys(todo)
-  let todos = todosIdx.map ( idx => todo[idx] )
-  return {
-    todos
-  }
-}
+  let todos = sortedAscending(todo);
+  return { todos };
+};
 
 const mapDispatchToProps = (dispatch) => ({
   requestTodos: () => dispatch(requestTodos())
-})
+});
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Schedule)
+)(Schedule);
--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -3,3 +3,19 @@ export const selectComments = (comments, commentIds) => {
   return(
   commentIds.map( id => comments[id])
 );};
+export const sortedAscending = (todo) => {
+  const todosIdx = Object.keys(todo);
+  let todos = todosIdx.map ( idx => todo[idx] );
+  let sortableDates = todosIdx.map (idx => {
+    if (todo[idx].dueMonth) {
+      return [idx, todo[idx].due_date];
+    }
+  });
+  sortableDates.sort( (a, b) => {
+    return new Date(a[1]) - new Date(b[1]);
+  });
+  sortableDates = sortableDates.filter( el => el !== undefined);
+  let sortedIdx = sortableDates.map( el => el[0] );
+  todos = sortedIdx.map ( idx => todo[idx] );
+  return todos;
+};
