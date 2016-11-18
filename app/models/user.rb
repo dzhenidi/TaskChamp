0a2958d99bfec9b6a8e334b2be60b8c8e74a405f
dzhenidi <dzhenidi@buffalo.edu>
2016-11-18 18:05:38 -0500

    add google events api

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,6 +66,7 @@ class User < ActiveRecord::Base
   end
 
   def self.from_omniauth(auth)
+    debugger
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
--- frontend/components/user/user_profile.jsx
+++ frontend/components/user/user_profile.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
+
 class UserProfile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       imageUrl: this.props.currentUser.avatarUrl || null,
-      imageFile: null
+      imageFile: null,
+      events: []
     };
 
     this.updateFile = this.updateFile.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.connectGoogle = this.connectGoogle.bind(this);
+    this.checkAuth = this.checkAuth.bind(this);
+    this.handleAuthResult = this.handleAuthResult.bind(this);
+    this.listUpcomingEvents = this.listUpcomingEvents.bind(this);
+    this.receiveEvents = this.receiveEvents.bind(this);
   }
 
   updateFile(e) {
@@ -31,6 +38,77 @@ class UserProfile extends React.Component {
     this.props.updateProfile(this.props.currentUser.id, formData);
   }
 
+  connectGoogle() {
+    const hasgapi = () => {
+            if (typeof (gapi) !== "undefined") {
+              this.checkAuth();
+            }
+            else {
+                window.setTimeout(function () {
+                    hasgapi();
+                }, 50);
+            }
+        }
+
+    return hasgapi();
+  }
+
+  checkAuth() {
+    gapi.auth.authorize(
+    {
+      'client_id': "973738423213-k7uovumgqvn7j2b1knf7lkb5lm331op7.apps.googleusercontent.com",
+      'scope': "https://www.googleapis.com/auth/calendar",
+      'immediate': true
+    }, this.handleAuthResult);
+  }
+
+  handleAuthResult (authResult) {
+    if (authResult && !authResult.error) {
+      this.loadCalendarApi();
+    } else {
+      //handle error
+    }
+  }
+
+  loadCalendarApi() {
+    gapi.client.load('calendar', 'v3', this.listUpcomingEvents)
+  }
+
+  listUpcomingEvents() {
+    // const request = gapi.client.calendar.events.list({
+    //   'calendarId': 'primary',
+    //   'timeMin': (new Date()).toISOString(),
+    //   'maxResults': 10,
+    //
+    // });
+    const event = {
+      'summary': 'hello from taskchamp',
+      'location': 'the pulpit',
+      'start': {
+        'dateTime': '2016-11-19T20:00:00-05:00'
+      },
+      'end': {
+        'dateTime': '2016-11-19T22:00:00-05:00'
+      },
+    };
+    const request = gapi.client.calendar.events.insert({
+      'calendarId': 'primary',
+      'resource': event
+    });
+
+    request.execute(resp => this.receiveEvents(resp));
+  }
+
+  receiveEvents(resp) {
+    this.setState({events: resp.items})
+  }
+
+  calendarEvents(){
+    return this.state.events.map( (event, idx) => (
+      <li id={idx}>{event.start.dateTime}: {event.summary}</li>
+    ));
+  }
+
   render() {
     return (
       <div>
@@ -47,6 +125,12 @@ class UserProfile extends React.Component {
             className="small home-button"
             >Save</button>
         </form>
+        <button
+          onClick={this.connectGoogle}>Google Calendar
+        </button>
+        <ul>
+          { this.state.events.length > 0 ? this.calendarEvents() : "no events" }
+        </ul>
       </div>
     );
   }
